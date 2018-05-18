d0418e316eef8cfead23eab248bee30bc77d0f86
Djurica Djuricic <p3ntaphr3ak@gmail.com>
2018-05-18 19:15:54 +0200

    Routing all set up. Generated home & logReg components.
    Routing is working in the best possible way, using routerLinks.

--- IT255-PZ/src/app/app.module.ts
+++ IT255-PZ/src/app/app.module.ts
@@ -1,28 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NavbarComponent} from './navbar/navbar.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LogRegComponent } from './log-reg/log-reg.component';
+
+const appRoutes: Routes =
+    [
+        {path: '', redirectTo: "/home", pathMatch: 'full'},
+        {path: 'home', component: HomeComponent},
+        {path: 'logReg', component: LogRegComponent}
+    ]
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        LoginComponent,
+        LogRegComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
